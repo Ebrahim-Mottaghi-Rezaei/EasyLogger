93d088a5959883bd74b7cb8756878b6da35492ed
ebrahim <e.mottaghi.rezaei@gmail.com>
2023-12-20 23:19:48 +0330

    Initial version.
    Icon: <a href="https://www.flaticon.com/free-icons/result" title="result icons">Result icons created by Parzival’ 1997 - Flaticon</a>

---
+++ Source/EasyLogger/Core/Logger.cpp
@@ -0,0 +1,60 @@
+﻿// Copyright Ebrahim Mottaghi Rezaei [https://www.linkedin.com/in/ebrahim-mr/]. All Rights Reserved.
+
+#include "Logger.h"
+#include "Engine.h"
+DEFINE_LOG_CATEGORY( EasyLogger );
+
+
+void ULogger::Log(const FString& Message, const ELogVerbosity::Type Verbosity, const EEasyLoggerType On, const float Duration) {
+	switch ( Verbosity ) {
+		case ELogVerbosity::Error:
+			if ( On != EEasyLoggerType::Console && GEngine )
+				GEngine->AddOnScreenDebugMessage( -1 , Duration , FColor::Red , *Message );
+
+			UE_LOG( EasyLogger , Error , TEXT("%s") , *Message );
+			break;
+		case ELogVerbosity::Warning:
+			if ( On != EEasyLoggerType::Console && GEngine )
+				GEngine->AddOnScreenDebugMessage( -1 , Duration , FColor::Orange , *Message );
+
+			UE_LOG( EasyLogger , Warning , TEXT("%s") , *Message );
+			break;
+		case ELogVerbosity::Display:
+			if ( On != EEasyLoggerType::Console && GEngine )
+				GEngine->AddOnScreenDebugMessage( -1 , Duration , FColor::White , *Message );
+
+			UE_LOG( EasyLogger , Display , TEXT("%s") , *Message );
+			break;
+		default: ;
+	}
+}
+
+
+void ULogger::LogInfo(UObject* Object, const FString& Message, const EEasyLoggerType On, const float Duration) {
+	LogInfoAnonymously( FString::Printf( TEXT( "[%s] %s" ) , *GetNameSafe( Object ) , *Message ) , On , Duration );
+}
+
+
+void ULogger::LogWarning(UObject* Object, const FString& Message, const EEasyLoggerType On, const float Duration) {
+	LogWarningAnonymously( FString::Printf( TEXT( "[%s] %s" ) , *GetNameSafe( Object ) , *Message ) , On , Duration );
+}
+
+
+void ULogger::LogError(UObject* Object, const FString& Message, const EEasyLoggerType On, const float Duration) {
+	LogErrorAnonymously( FString::Printf( TEXT( "[%s] %s" ) , *GetNameSafe( Object ) , *Message ) , On , Duration );
+}
+
+
+void ULogger::LogInfoAnonymously(const FString& Message, const EEasyLoggerType On, const float Duration) {
+	Log( Message , ELogVerbosity::Type::Display , On , Duration );
+}
+
+
+void ULogger::LogWarningAnonymously(const FString& Message, const EEasyLoggerType On, const float Duration) {
+	Log( Message , ELogVerbosity::Type::Warning , On , Duration );
+}
+
+
+void ULogger::LogErrorAnonymously(const FString& Message, const EEasyLoggerType On, const float Duration) {
+	Log( Message , ELogVerbosity::Type::Error , On , Duration );
+}
---
+++ Source/EasyLogger/Core/Logger.h
@@ -0,0 +1,40 @@
+﻿// Copyright Ebrahim Mottaghi Rezaei [https://www.linkedin.com/in/ebrahim-mr/]. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Logger.generated.h"
+DECLARE_LOG_CATEGORY_EXTERN( EasyLogger , All , All );
+
+UENUM( BlueprintType , Category="Easy Logger" )
+enum class EEasyLoggerType :uint8 {
+	Screen  = 0 UMETA( DisplayName = "Screen" ),
+	Console = 1 UMETA( DisplayName = "Console" ),
+	Both    = 2 UMETA( DisplayName = "Both" ),
+};
+
+UCLASS()
+class EASYLOGGER_API ULogger : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+	static void Log(const FString& Message, const ELogVerbosity::Type Verbosity, const EEasyLoggerType On, const float Duration);
+
+public:
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogInfo(UObject* Object, const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Screen, const float Duration = 3.0f);
+
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogWarning(UObject* Object, const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Both, const float Duration = 5.0f);
+
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogError(UObject* Object, const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Both, const float Duration = 10.0f);
+
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogInfoAnonymously(const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Screen, const float Duration = 3.0f);
+
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogWarningAnonymously(const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Both, const float Duration = 5.0f);
+
+	UFUNCTION( BlueprintCallable , Category = "Easy Logger" )
+	static void LogErrorAnonymously(const FString& Message, const EEasyLoggerType On = EEasyLoggerType::Both, const float Duration = 10.0f);
+};
